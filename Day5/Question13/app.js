647c9ca6647c49792858fa5d5328d7af1557ce5a
Tehreem-Asghar <161032033+Tehreem-Asghar@users.noreply.github.com>
2024-03-06 12:03:48 +0500

    first commit

---
+++ Day5/Question13/app.js
@@ -0,0 +1,9 @@
+"use strict";
+//Question 13: Your Own Array: Think of your favorite mode of transportation, 
+//such as a motorcycle or a car, and make a list that stores several examples. 
+//Use your list to print a series of statements about these items,
+// such as “I would like to own a Honda motorcycle.”
+let transportations = ['Motorcycle', 'Bike', 'Car', 'Electic Train'];
+for (let transport of transportations) {
+    console.log(`I would like to own a ${transport}.`);
+}
---
+++ Day5/Question15/app.js
@@ -0,0 +1,13 @@
+"use strict";
+//Question 15: Changing Guest List: One of your guests can't make it to the dinner,
+//so you need to send out a new set of invitations with a replacement guest.
+let Guest_list = ['Arham', 'Taha', 'sarim', 'Daniyal'];
+for (let Guest of Guest_list) {
+    console.log(`Dear ${Guest}: would you like to join me for dinner?`);
+}
+console.log(`Arham can't make it to dinner.`);
+let new_guest = "Zeshan";
+Guest_list[0] = new_guest;
+for (let guest of Guest_list) {
+    console.log(`Dear ${guest} would you like to join me for dinner?`);
+}
