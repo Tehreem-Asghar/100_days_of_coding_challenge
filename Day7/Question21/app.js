60eef656de0c486d25b2bf1dddbf55a8f8be1514
Tehreem-Asghar <161032033+Tehreem-Asghar@users.noreply.github.com>
2024-03-08 10:13:14 +0500

    first commit

---
+++ Day7/Question21/app.js
@@ -0,0 +1,9 @@
+"use strict";
+// Question 21: Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+// Explain & TIP: Objects in TypeScript can store various related data about a single entity. Think of an object as a container for characteristics of something, like a book or a car.
+let person = {
+    Name: "Arham",
+    country: "Dubai",
+    age: 19,
+};
+console.log(person);
