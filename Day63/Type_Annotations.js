dabfe75e4cabb48e5426866150db9cf0aac1c141
Tehreem-Asghar <161032033+Tehreem-Asghar@users.noreply.github.com>
2024-05-04 01:37:18 +0500

    Day 64 Done

--- Day63/Type_Annotations.js
+++ Day63/Type_Annotations.js
@@ -1,8 +1,14 @@
 // Type Annotations
+// Defined explicitly specify the type of variables
 var Name = "Tehreem";
 var number = 324656;
 var array = ["a", "b", "c", "d"];
-function food(item, item1) {
-    console.log(item, item1);
-}
-food("biryani", "chicken");
+console.log(Name);
+console.log(number);
+console.log(array);
+var a;
+var b;
+a = "Ayesha";
+b = 123;
+console.log(a);
+console.log(b);
---
+++ Day64/Type-Annotations-With-Objects.js
@@ -0,0 +1,19 @@
+// TYPE ANNOTATIONS WITH OBJECT
+// Declear variable object with type annotations
+var person;
+// initilaized a object
+person = {
+    name: "Tehreem",
+    // f_name : 890, Error : Type 'number' is not assignable to type 'string'.
+    f_name: "Asghar",
+    city: "Karachi",
+    roll_number: 1234,
+};
+console.log(person);
+//we can also use another mthod
+var course = {
+    course: 'html',
+    fee: 5000,
+    confirm: true
+};
+console.log(course);
