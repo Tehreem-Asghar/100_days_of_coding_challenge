63c265f9e6ca9e7ce84141afddac5771c5849d67
Tehreem-Asghar <161032033+Tehreem-Asghar@users.noreply.github.com>
2024-05-03 22:37:15 +0500

    Day 63 Done

---
+++ Day63/Type_Annotations.js
@@ -0,0 +1,8 @@
+// Type Annotations
+var Name = "Tehreem";
+var number = 324656;
+var array = ["a", "b", "c", "d"];
+function food(item, item1) {
+    console.log(item, item1);
+}
+food("biryani", "chicken");
---
+++ Day63/dynamic_type_determination.js
@@ -0,0 +1,4 @@
+// Dynamic Type Determination
+var nam_e = "Arham"; // TypeScript infers the type of the variable as string
+console.log(nam_e);
+// nam_e = 1246 ;  Error: Type 'number' is not assignable to type 'string'.
