3b4425ad7af866375a780abd93402bd594c1b563
Tehreem-Asghar <161032033+Tehreem-Asghar@users.noreply.github.com>
2024-05-07 00:41:51 +0500

    Done

---
+++ Day67/Annotations_With_Multidimensional.js
@@ -0,0 +1,7 @@
+console.log("*******************************************\nType Annotations With Multidimensional Arrays\n***********************************************");
+var stringType = ["a", "b", "c", "d"];
+console.log('stringType :', stringType);
+var numberType = [1, 2, 3, 4, 5];
+console.log('numberType : ', numberType);
+var multipuleTypes = [10, false, "Apple", true];
+console.log('MultipuleType :', multipuleTypes);
---
+++ Day67/Type_Annotations_With_Arrays.js
@@ -0,0 +1,5 @@
+console.log("*****************************\nType Annotations With Arrays\n****************************");
+var studentNames = ['Tehreem', 'Hajra', 'Shaheen', 'Humail', 'namal'];
+for (var i = 0; i < studentNames.length; i++) {
+    console.log(studentNames[i]);
+}
