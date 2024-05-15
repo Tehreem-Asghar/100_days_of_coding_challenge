0f79808d370c8ea6e211280d00ac081b362fd3f5
Tehreem-Asghar <161032033+Tehreem-Asghar@users.noreply.github.com>
2024-05-15 11:13:07 +0500

    Done

---
+++ Day74/01-debugging.js
@@ -0,0 +1,16 @@
+"use strict";
+/*************
+ * DEBUGGING *
+ *************/
+class Person {
+    constructor(greet) {
+        this.greet = greet;
+    }
+    Name(name) {
+        console.log(`${this.greet}, ${name}`);
+    }
+}
+let person = new Person('hello');
+console.log(person);
+person.Name('tehreem');
+//# sourceMappingURL=01-debugging.js.map
